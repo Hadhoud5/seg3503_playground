ca0ad7256136d37c5b0f1ea0c9bc46ed11f3fde5
Hadhoud5 <55165009+Hadhoud5@users.noreply.github.com>
2021-06-10 21:43:26 +0100

    Commit 5 : Test de taille ajoutée et test pour montrer la position

--- Lab04/tic_java/src/Tic.java
+++ Lab04/tic_java/src/Tic.java
@@ -17,3 +17,30 @@ public class Tic {
     }
     return board;
   }
+
+   public int size() {
+    return rows*cols;
+  }
+
+}
+
+
+public String showPositions() {
+    String board = "";
+    int x = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (j == 0) {
+          board += x++;
+        } else {
+          board += "|" + x++;
+        }
+      }
+      if (rows > 1 && i != rows - 1) {
+        board += "\n";
+      }
+    }
+    return board;
+  }
+
+}
