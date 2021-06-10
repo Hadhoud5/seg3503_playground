31bbf927cdf336a13b707108a9ab9228f628bb66
Hadhoud5 <55165009+Hadhoud5@users.noreply.github.com>
2021-06-10 23:42:32 +0100

    Méthode "calculate_grade" ajoutée

--- Assignment_2/Partie 2/grades/lib/grades/calculator.ex
+++ Assignment_2/Partie 2/grades/lib/grades/calculator.ex
@@ -4,7 +4,7 @@ defmodule Grades.Calculator do
 
     avg_labs = avg(labs)
 
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
     round(mark * 100)
   end
 
@@ -23,7 +23,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> "A+"
@@ -57,7 +57,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> 10
@@ -98,3 +98,14 @@ def failed_to_participate?(avg_homework, avg_exams, num_labs) do
   end
 end
 
+
+
+""" La méthode "calculate_grade" retourne la note de l'étudiant """
+
+
+
+
+  def calculate_grade(avg_labs, avg_homework, midterm, final) do
+    0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+  end
+end
